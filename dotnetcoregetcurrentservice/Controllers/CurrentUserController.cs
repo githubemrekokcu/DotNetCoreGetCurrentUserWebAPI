11f74645e7a56cd6af9d2e325f2c5555dc4996e6
Developer <Developer@DESKTOP-QFIB5MA>
2019-12-29 16:01:07 +0300

    Add project files.

---
+++ dotnetcoregetcurrentservice/Controllers/CurrentUserController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http.Features;
+
+namespace dotnetcoregetcurrentservice.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CurrentUserController : ControllerBase
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public CurrentUserController(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        [HttpGet]
+        public ActionResult<String> Get()
+        {
+            var context = _httpContextAccessor.HttpContext.User.Identity.Name.Split('\\')[1];
+            return "setUser(\"" + context + "\");";
+        }
+    }
+}
